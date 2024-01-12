6f646506017289a5c748a1deed85cab5da91a008
NGOMA SITA <dorian.ngoma-sita@capgemini.com>
2024-01-12 13:57:02 +0100

    test

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const jsonServer = require('json-server');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Définir le routeur JSON Server
-const router = jsonServer.router(path.join(__dirname, 'storage/bd.json'));
+const router = jsonServer.router(path.join(__dirname, 'src/main/storage/bd.json'));
 
+app.use(cors());
 // Utiliser le middleware JSON Server
 app.use('/api', router);
 
@@ -17,3 +19,5 @@ app.use('/api', router);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports=app
